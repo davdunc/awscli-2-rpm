dffa59e485c593bf1c4b2b1a794eb5398ee7ce81
David Duncan <davdunc@amazon.com>
2021-10-25 14:58:56 -0700

    rename to reflect config

---
+++ collect-dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -sSL --output pyinstaller-4.2.tar.gz https://github.com/pyinstaller/pyinstaller/releases/download/v4.2/pyinstaller-4.2.tar.gz
+curl -sSL --output aws-crt-python-0.12.6.tar.gz https://github.com/awslabs/aws-crt-python/archive/refs/tags/v0.12.6.tar.gz
