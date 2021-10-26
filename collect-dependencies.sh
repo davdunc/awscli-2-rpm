d73c5e8ed9e006cc3b553fe68f2532183408eb51
David Duncan <davdunc@amazon.com>
2021-10-26 05:49:20 -0700

    add awscli to downloads

--- collect-dependencies.sh
+++ collect-dependencies.sh
@@ -2,3 +2,5 @@
 
 curl -sSL --output pyinstaller-4.2.tar.gz https://github.com/pyinstaller/pyinstaller/releases/download/v4.2/pyinstaller-4.2.tar.gz
 curl -sSL --output aws-crt-python-0.12.6.tar.gz https://github.com/awslabs/aws-crt-python/archive/refs/tags/v0.12.6.tar.gz
+curl -sSL --output awscli-2.3.0.tar.gz https://github.com/aws/aws-cli/archive/refs/tags/2.3.0.tar.gz
+mv 2.3.0
